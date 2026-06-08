3781cb21f934280225cda15536392170a945f949
yasmirandaa <20241ctb0100077@estudantes.ifpr.edu.br>
2026-06-08 19:47:31 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

---
+++ portfolio-angular/src/app/contato/contato.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  imports: [],
+  templateUrl: './contato.html',
+  styleUrl: './contato.css',
+})
+export class Contato {}
---
+++ portfolio-angular/src/app/projetos/projetos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projetos',
+  imports: [],
+  templateUrl: './projetos.html',
+  styleUrl: './projetos.css',
+})
+export class Projetos {}
