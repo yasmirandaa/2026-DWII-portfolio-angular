68fdcb4906f8bb6e98645eb9445321b1db4aef11
yasmirandaa <20241ctb0100077@estudantes.ifpr.edu.br>
2026-08-03 19:51:40 +0000

    conceito C a arrumar

--- portfolio-angular/src/app/projetos/projetos.ts
+++ portfolio-angular/src/app/projetos/projetos.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ProjetoService, Projeto } from '../projeto.service';
 
 @Component({
   selector: 'app-projetos',
-  imports: [MatCardModule],
-  templateUrl: './projetos.html',
-  styleUrl: './projetos.css',
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
+  templateUrl: './projetos.html'
 })
-export class Projetos {}
+export class Projetos implements OnInit {
+  private service = inject(ProjetoService);
+
+  projetos: Projeto[] = [];
+  carregando = true;
+  erro = '';
+
+  ngOnInit() {
+    this.service.listar().subscribe({
+      next: (lista) => {
+        this.projetos = lista;
+        this.carregando = false;
+      },
+      error: () => {
+        this.erro = 'Falha ao carregar os projetos.';
+        this.carregando = false;
+      }
+    });
+  }
+}
